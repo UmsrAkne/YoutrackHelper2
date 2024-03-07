c3d8e3c94d87c672732ecb77f11db3678ef07bbf
Akn <sibata486@gmail.com>
2024-03-07 19:39:28 +0900

    init

---
+++ YoutrackHelper2/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Prism.Mvvm;
+
+namespace YoutrackHelper2.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        public string Title => "Prism Application";
+    }
+}
