02b313ab2f52e48f26bcfde5c360cc4f68ed13c5
Akn <sibata486@gmail.com>
2024-03-07 19:43:44 +0900

    add files / ProjectList のビューと関連するファイルを追加

---
+++ YoutrackHelper2/ViewModels/ProjectListViewModel.cs
@@ -0,0 +1,9 @@
+using Prism.Mvvm;
+
+namespace YoutrackHelper2.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class ProjectListViewModel : BindableBase
+    {
+    }
+}
