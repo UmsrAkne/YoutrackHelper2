363527e2f454fdf4c1e12a0900ed0c357fc5f158
Akn <sibata486@gmail.com>
2024-03-10 14:06:27 +0900

    add view / 課題リストページへの遷移を実装

--- YoutrackHelper2/ViewModels/MainWindowViewModel.cs
+++ YoutrackHelper2/ViewModels/MainWindowViewModel.cs
@@ -1,10 +1,25 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+using YoutrackHelper2.Views;
 
 namespace YoutrackHelper2.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class MainWindowViewModel : BindableBase
     {
+        private readonly IRegionManager regionManager;
+
+        public MainWindowViewModel(IRegionManager regionManager)
+        {
+            this.regionManager = regionManager;
+        }
+
         public string Title => "Prism Application";
+
+        public DelegateCommand NavigateToIssueListPageCommand => new (() =>
+        {
+            regionManager.RequestNavigate("ContentRegion", nameof(IssueList));
+        });
     }
 }
