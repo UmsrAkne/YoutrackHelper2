430a966b98f71830ac26d53d0b30d55d3a4033f8
Akn <sibata486@gmail.com>
2024-05-05 16:11:26 +0900

    add / リリースビルド時にバージョンを表示するようにした

--- YoutrackHelper2/Models/TitleBarText.cs
+++ YoutrackHelper2/Models/TitleBarText.cs
@@ -5,7 +5,14 @@ namespace YoutrackHelper2.Models
     public class TitleBarText : BindableBase
     {
         private string text;
+        private string version;
 
-        public string Text { get => text; set => SetProperty(ref text, value); }
+        public string Text
+        {
+            get => $"{text} {Version}";
+            set => SetProperty(ref text, value);
+        }
+
+        public string Version { get => version; set => SetProperty(ref version, value); }
     }
 }
