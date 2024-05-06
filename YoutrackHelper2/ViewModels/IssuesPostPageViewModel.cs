3ac93ceae24cf50758813e631aab0e05ac7b5d29
Akn <sibata486@gmail.com>
2024-05-06 19:27:13 +0900

    add / 複数課題の同時投稿ページの UI を設置

--- YoutrackHelper2/ViewModels/IssuesPostPageViewModel.cs
+++ YoutrackHelper2/ViewModels/IssuesPostPageViewModel.cs
@@ -1,16 +1,26 @@
 using System;
+using System.Collections.ObjectModel;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
+using YoutrackHelper2.Models;
 
 namespace YoutrackHelper2.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class IssuesPostPageViewModel : BindableBase, IDialogAware
     {
+        private ObservableCollection<IssueWrapper> issueWrappers = new ObservableCollection<IssueWrapper>();
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => string.Empty;
+
+        public ObservableCollection<IssueWrapper> IssueWrappers
+        {
+            get => issueWrappers;
+            set => SetProperty(ref issueWrappers, value);
+        }
 
         public DelegateCommand CloseCommand => new DelegateCommand(() =>
         {
