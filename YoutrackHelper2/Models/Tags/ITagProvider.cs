8d6b4db0cd0322a383a7398f8502e5b5df06ad59
Akn <sibata486@gmail.com>
2024-06-11 22:39:45 +0900

    add feat / サーバー上に存在する全てのタグを取得する機能を実装

--- YoutrackHelper2/Models/Tags/ITagProvider.cs
+++ YoutrackHelper2/Models/Tags/ITagProvider.cs
@@ -1,7 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace YoutrackHelper2.Models.Tags
 {
     public interface ITagProvider
     {
-        void SetConnection(string uri, string token);
+        void SetConnection(string url, string tokenStr);
+
+        Task<List<Tag>> GetTags();
     }
 }
--- YoutrackHelper2/Models/Tags/TagProvider.cs
+++ YoutrackHelper2/Models/Tags/TagProvider.cs
@@ -1,9 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
 namespace YoutrackHelper2.Models.Tags
 {
     public class TagProvider : ITagProvider
     {
-        public void SetConnection(string uri, string token)
+        private string uri;
+        private string token;
+
+        public void SetConnection(string url, string tokenStr)
         {
+            uri = $"{url}/api";
+            token = tokenStr;
+        }
+
+        public async Task<List<Tag>> GetTags()
+        {
+            using var client = new HttpClient();
+            client.BaseAddress = new Uri(uri);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            var response = await client.GetAsync("api/tags?fields=id,name");
+            response.EnsureSuccessStatusCode();
+
+            var responseBody = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<List<Tag>>(responseBody);
         }
     }
 }
