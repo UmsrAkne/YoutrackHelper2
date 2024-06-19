d5ff3c1ecebe5571826b6c42f43183d6bfcbfe07
Akn <sibata486@gmail.com>
2024-06-19 15:21:45 +0900

    add doc / Enum の各値にドキュメンテーションコメントを追加。

--- YoutrackHelper2/Models/State.cs
+++ YoutrackHelper2/Models/State.cs
@@ -2,9 +2,24 @@ namespace YoutrackHelper2.Models
 {
     public enum State
     {
-       Incomplete,
-       Completed,
-       Progressing,
-       Obsolete,
+        /// <summary>
+        /// タスクが未完了の状態です。
+        /// </summary>
+        Incomplete,
+
+        /// <summary>
+        /// タスクが完了した状態です。
+        /// </summary>
+        Completed,
+
+        /// <summary>
+        /// タスクが作業中の状態です。
+        /// </summary>
+        Progressing,
+
+        /// <summary>
+        /// タスクが廃止された状態です。
+        /// </summary>
+        Obsolete,
     }
 }
--- YoutrackHelper2/Models/WorkType.cs
+++ YoutrackHelper2/Models/WorkType.cs
@@ -2,10 +2,29 @@ namespace YoutrackHelper2.Models
 {
     public enum WorkType
     {
+        /// <summary>
+        /// 機能に関する作業です。
+        /// </summary>
         Feature,
+
+        /// <summary>
+        /// 軽微な作業につけるタイプです。
+        /// </summary>
         Todo,
+
+        /// <summary>
+        /// 外観に関する変更につけるタイプです。
+        /// </summary>
         Appearance,
+
+        /// <summary>
+        /// バグ修正タスクです。
+        /// </summary>
         Bug,
+
+        /// <summary>
+        /// テストに関するタスクです。
+        /// </summary>
         Test,
     }
 }
