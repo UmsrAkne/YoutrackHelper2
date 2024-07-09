acd3e8209c81405f6836a21a9239a95e802bc1f6
Akn <sibata486@gmail.com>
2024-07-09 17:48:11 +0900

    Update version number.

--- YoutrackHelper2/ViewModels/MainWindowViewModel.cs
+++ YoutrackHelper2/ViewModels/MainWindowViewModel.cs
@@ -140,7 +140,7 @@ namespace YoutrackHelper2.ViewModels
         private void SetVersion()
         {
             // リリースビルドの場合のみ実行するコード
-            TitleBarText.Version = "version : " + "20240702" + "a";
+            TitleBarText.Version = "version : " + "20240709" + "a";
         }
     }
 }
