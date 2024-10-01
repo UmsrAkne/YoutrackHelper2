9b4d76a2c0348f16647c25e6948b2dd5aadda664
Akn <sibata486@gmail.com>
2024-10-01 21:47:40 +0900

    refactoring / 余分な記述の削除、 SortCriteria のメンバーへのドキュメントコメントの追加。

--- YoutrackHelper2/Models/SortCriteria.cs
+++ YoutrackHelper2/Models/SortCriteria.cs
@@ -1,9 +1,23 @@
 namespace YoutrackHelper2.Models
 {
+    /// <summary>
+    /// ソートの基準を表す Enum です。
+    /// </summary>
     public enum SortCriteria
     {
+        /// <summary>
+        /// 日時を基準にソートします。
+        /// </summary>
         Date,
+
+        /// <summary>
+        /// アルファベット（五十音）でソートします。
+        /// </summary>
         Alphabetical,
+
+        /// <summary>
+        /// 課題の ID を基準にソートします。
+        /// </summary>
         Id,
     }
 }
