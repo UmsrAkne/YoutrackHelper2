ad0c48b979ce48ac315b027e72a56c9732cba6de
Akn <sibata486@gmail.com>
2024-12-15 23:12:15 +0900

    add / DetailedIssuePostPage の UI をビューモデルのプロパティとバインディングした。

--- YoutrackHelper2/ViewModels/DetailedIssuePostPageViewModel.cs
+++ YoutrackHelper2/ViewModels/DetailedIssuePostPageViewModel.cs
@@ -2,15 +2,23 @@ using System;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
+using YoutrackHelper2.Models;
 
 namespace YoutrackHelper2.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class DetailedIssuePostPageViewModel : BindableBase, IDialogAware
     {
+        private IssueWrapper issueWrapper;
+        private string tagsText;
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => string.Empty;
+
+        public IssueWrapper IssueWrapper { get => issueWrapper; private set => SetProperty(ref issueWrapper, value); }
+
+        public string TagsText { get => tagsText; set => SetProperty(ref tagsText, value); }
 
         public DelegateCommand CloseCommand => new DelegateCommand(() =>
         {
@@ -25,6 +33,7 @@ namespace YoutrackHelper2.ViewModels
 
         public void OnDialogOpened(IDialogParameters parameters)
         {
+            IssueWrapper = parameters.GetValue<IssueWrapper>(nameof(IssueWrapper));
         }
     }
 }
